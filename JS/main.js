ef402f2b22a9db562120c9465272c2c3fb8db2d6
AzizIo <azizbekmadzitov@gmail.com>
2026-01-17 14:54:30 +0300

    Теперь картинки в галярее меняютсья по кнопкам

---
+++ JS/main.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+    const bth = document.querySelector(".bth");
+    bth.addEventListener("mouseenter", () =>{
+        gsap.to(bth, {
+            scale: 1.05,
+            duration: 0.3
+        });
+
+    });
+    bth.addEventListener("mouseleave", () =>{
+         gsap.to(bth, {
+            scale: 1,
+            duration: 0.3
+        });
+    });
+   
+
+});
